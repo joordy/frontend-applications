331efe06f9d0cbc8f3ee87a73a2250c802659903
joordy <jmfronik@gmail.com>
2020-11-18 12:20:17 +0100

    connected two apis

--- src/modules/cleanData.js
+++ src/modules/cleanData.js
@@ -5,5 +5,24 @@ export async function cleanMyData() {
   const datasrc1 = await receiveData(apiOne)
   const datasrc2 = await receiveData(apiTwo)
 
-  console.log(datasrc1, datasrc2)
+  // console.log(datasrc1, datasrc2)
+
+  const result = datasrc1.map((api1) => {
+    const api2 = datasrc2.find((api2) => api1.areaid === api2.areaid)
+    api1.spec = api2
+    return api1
+  })
+
+  // const api2 = datasrc2.find((api2) => api1.areaid === api2.areaid)
+  // const api2 = datasrc2.find(api2 => api1.areaid === api2.areaid) api1 = api2 return api1
+
+  // dataset1.map((location) => {
+  //   const parkingSpecs = dataset2.find(
+  //     (parkingSpecs) => location.areaid === parkingSpecs.areaid
+  //   )
+  //   location.parkingSpecs = parkingSpecs
+  //   return location
+  // })
+
+  console.log(result)
 }
--- src/modules/helpers/rdwEntry.js
+++ src/modules/helpers/rdwEntry.js
@@ -1,2 +1,2 @@
-export const apiOne = 'https://opendata.rdw.nl/resource/b3us-f26s.json'
-export const apiTwo = 'https://opendata.rdw.nl/resource/t5pc-eb34.json'
+export const apiOne = 'https://opendata.rdw.nl/resource/t5pc-eb34.json'
+export const apiTwo = 'https://opendata.rdw.nl/resource/b3us-f26s.json'
