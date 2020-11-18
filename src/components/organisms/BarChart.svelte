2e63c7a35ba9b5b75b63ef3e0204222504302e6e
joordy <jmfronik@gmail.com>
2020-11-18 21:54:24 +0100

    imported D3

--- src/components/organisms/BarChart.svelte
+++ src/components/organisms/BarChart.svelte
@@ -1,6 +1,27 @@
 <script>
+  import { onMount } from 'svelte'
   export let data
   console.log(data)
+  let carData = data.map((item) => item.carCapacity)
+  let elements
+  console.log(carData)
+
+  onMount(() => {
+    const svg = select('svg')
+
+    svg
+      .select(elements)
+      .selectAll('rect')
+      .data(carData)
+      .enter()
+      .append('rect')
+      .style('width', function (d) {
+        return d + 'px'
+      })
+      .text(function (d) {
+        return d
+      })
+  })
 </script>
 
 <style>
@@ -10,3 +31,5 @@
 </style>
 
 <h1>{JSON.stringify(data)}</h1>
+
+<svg bind:this={elements} />
