f84c62610198cbb20310780937906aeb1761ebe6
joordy <jmfronik@gmail.com>
2020-11-24 18:18:57 +0100

    Cleaning pattern to make total counts, thanks to @veerleprins

--- src/modules/helpers/utils.js
+++ src/modules/helpers/utils.js
@@ -30,7 +30,7 @@ export const filterData = (data) => {
         item && item.disabledaccess ? item.disabledaccess : 0
       ),
       maxDriveThrough: parseInt(
-        item && item.maximumvehicleheight ? item.maximumvehicleheight : 0
+        item && item.maximumvehicleheight ? item.maximumvehicleheight : 430
       ),
       lat: parseFloat(
         item && item.location.latitude ? item.location.latitude : 0
@@ -50,39 +50,81 @@ const getCityName = (parkingName) => {
     let name = parkingName
     let regPattern = /\(([^)]+)\)/
     let cityname = regPattern.exec(name)[1]
-    return cityname
+    return cityname.toLowerCase()
   } else {
     return null
   }
 }
 
-export const countValues = (dataset) => {
-  console.log('hello', dataset)
-
-  // let newArr = []
-  // dataset.forEach((capacity) => {
-  //   if (newArr.find((item) => item.carCapacity == capacity) == undefined) {
-  //     newArr.push({
-  //       carCapacity: capacity,
-  //       value: 0,
-  //     })
-  //   }
-  //   newArr.find((item) => item.carCapacity == capacity).value += 1
-  // })
-
-  // console.log(newArr)
-}
-
 export const checkForValue = (value) => {
-  if (value === 'carCapacity') {
+  if (value === 'totalCapacity') {
     value = 'parkeer capaciteit'
     return value
-  } else if (value === 'maxDriveThrough') {
+  } else if (value === 'avgDriveThrough') {
     value = 'maximale doorrijhoogte'
     return value
-  } else if (value === 'evChargerCapacity') {
-    value = 'laadpalen'
+  } else if (value === '') {
+    value = 'chargingPoints'
     return value
   }
   return value
+}
+
+export const countValues = (dataset) => {
+  const newData = filterCities(dataset)
+  const finalData = mergeDataCity(dataset, newData)
+  return finalData
+}
+
+const filterCities = (dataset) => {
+  let newData = []
+  dataset.forEach((item) => {
+    let i = newData.findIndex((x) => x.cityName == item.city)
+    if (i <= -1) {
+      newData.push({ cityName: item.city })
+    }
+  })
+  return newData
+}
+
+const mergeDataCity = (dataset, newData) => {
+  const uniqueCities = newData.map((item) => item.cityName)
+  const allData = createObj(uniqueCities)
+  parseInfo(allData, dataset)
+  return allData
+}
+
+// Made with the expertise of @veerleprins
+const createObj = (allCities) => {
+  let countObj
+  return allCities.map((city) => {
+    return (countObj = {
+      name: city,
+      totalCapacity: 0,
+      avgDriveThrough: 0,
+      chargingPoints: 0,
+    })
+  })
+}
+
+const parseInfo = (a, dataset) => {
+  let num
+  let avg
+  let num2
+  a.forEach((obj) => {
+    num = 0
+    avg = 0
+    num2 = 0
+    dataset.forEach((totalObj) => {
+      if (obj.name === totalObj.city) {
+        num += totalObj.carCapacity
+        avg += totalObj.maxDriveThrough
+        num2 += totalObj.evChargerCapacity
+      }
+    })
+    let newAvg = avg / dataset.length
+    obj.totalCapacity = num
+    obj.avgDriveThrough = newAvg
+    obj.chargingPoints = num2
+  })
 }
